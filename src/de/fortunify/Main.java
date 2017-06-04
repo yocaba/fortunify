8ac4df59416669500d97df67bcb4b79ff6944857
Doreen Seider <seider.doreen@gmail.com>
2017-06-04 20:08:34 +0200

    made source and sink configurable (passed as arguments)

--- src/de/fortunify/Main.java
+++ src/de/fortunify/Main.java
@@ -1,6 +1,7 @@
 package de.fortunify;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,22 +17,35 @@ import de.fortunify.spi.FortuneGenerator;
 
 public class Main {
 
+    private static final String ERROR_MESSAGE_ARGS = String.format("Two arguments expected: fortune source %s and fortune sink %s", Main.enumToLogMessage(FortuneSource.class), Main.enumToLogMessage(FortuneSink.class));
+    
     private static final Logger logger = LoggerFactory.getLogger(Main.class.getSimpleName());
 
     private static final String DELIMITER = ",";
 
     private static final int MAX_CHARS = 140;
 
-    private Main() {
+    private Main() {}
+    
+    private static <E extends Enum<E>> String enumToLogMessage(Class<E> enumClass) {
+        return Arrays.toString(enumClass.getEnumConstants()).toLowerCase().replaceAll(", ", "|");
     }
 
     /**
      * @param args
      *            fist argument depicts the fortune source, second argument
-     *            depicts the fortune sinks (comma-separated list)
+     *            depicts the fortune sink (comma-separated list)
      */
     public static void main(String[] args) {
 
+        if (args.length != 2) {
+            if (args.length < 2) {
+                logger.error(ERROR_MESSAGE_ARGS);
+                System.exit(1);
+            } else {
+                logger.warn(ERROR_MESSAGE_ARGS);
+            }
+        }
         FortuneGenerator fortuneGenerator;
         try {
             fortuneGenerator = Main.createFortuneGenerator(args[0]);
